bca4e68ecc82d9fa5f1d0b276bc91be8faeb50c1
Jan Schill <schill@hey.com>
2023-01-15 15:27:24 +0100

    WIP Plot Temp

--- docs/main.js
+++ docs/main.js
@@ -1,10 +1,9 @@
 import participants from "./data/participants.json" assert { type: "json" };
+import globalMeanTemps from "./data/global-mean-temp-land-ocean.json" assert { type: "json" };
 
 document.addEventListener("DOMContentLoaded", () => {
-  console.log("DOM loaded");
-  console.log(participants[0])
-
-  const body = document.querySelector("body");
-  body.innerHTML = participants[0].name
-
+  const header = document.querySelector(".main-header");
+  const headerBody = header.querySelector(".main-header__body");
+  const currentTemperature = globalMeanTemps[globalMeanTemps.length - 1]
+  headerBody.innerHTML = `${currentTemperature.lowess5} °C`
 })
