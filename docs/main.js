1937251455f390ae6891daa12b1e4d9f451fed63
Jan Schill <schill@hey.com>
2023-01-15 15:46:42 +0100

    Plot Chart

--- docs/main.js
+++ docs/main.js
@@ -6,4 +6,42 @@ document.addEventListener("DOMContentLoaded", () => {
   const headerBody = header.querySelector(".main-header__body");
   const currentTemperature = globalMeanTemps[globalMeanTemps.length - 1]
   headerBody.innerHTML = `${currentTemperature.lowess5} °C`
+
+  const globalMeanTempChart = document.getElementById("globalMeanTempChart");
+  new Chart(globalMeanTempChart, {
+    type: "line",
+    data: {
+      labels: globalMeanTemps.map(t => t.year),
+      datasets: [{
+        data: globalMeanTemps.map(t => t.lowess5),
+      }]
+    },
+    options: {
+      plugins: {
+        legend: {
+          display: false
+        }
+      },
+      tension: 0.5,
+      elements: {
+        point: {
+          radius: 0
+        }
+      },
+      scales: {
+        x: [{
+          display: false,
+          ticks: {
+            display: false //this will remove only the label
+          }
+        }],
+        y: [{
+          display: false,
+          ticks: {
+            display: false //this will remove only the label
+          }
+        }]
+      }
+    }
+  });
 })
